b959f75c5c7480a15dfb9a1af049531c44400fd7
unknown <1045354578@qq.com>
2020-04-18 19:04:58 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <i class="iconfont">&#xe65a;</i>
+    <font-awesome-icon :icon="['fas', 'square']" /> -->
+    <river-selection></river-selection>
+  </div>
+</template>
+
+<script>
+import RiverSelection from './components/Selection/RiverSelection'
+
+export default {
+  name: 'App',
+  components: {
+    RiverSelection
+  }
+}
+</script>
+
+<style>
+  @import './assets/css/base.css';
+</style>
---
+++ src/components/Selection/RiverSelection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sel-cbx">
+    <div class="sel-header">
+      <i class="iconfont">&#xe65a;</i>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'RiverSelection',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .sel-cbx{
+    width:220px;
+    height:40px;
+    background: black;
+    opacity: 0.8;
+    border-radius: 6px;
+    display: table-cell;
+    vertical-align: middle;;
+  }
+  .sel-header{
+    color: white;
+    margin: 5px;
+  }
+</style>
+
+
+
